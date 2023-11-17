d26450b119ded0c0275ca09360e70e90467ec063
Bohdan Rudenko <brudenko2804@gmail.com>
2023-11-17 17:06:59 +0200

    Add and implement the basic functionality of the Location and Island classes

--- src/main/java/ua/javarush/island/Island.java
+++ src/main/java/ua/javarush/island/Island.java
@@ -1,4 +1,24 @@
 package ua.javarush.island;
 
 public class Island {
+    private final Location[][] locations;
+
+    public Island(int x, int y) {
+        locations = new Location[x][y];
+    }
+
+    public enum Direction{
+        LEFT(1,0),
+        RIGHT(-1, 0),
+        UP(0, 1),
+        DOWN(0, -1);
+
+        private int x;
+        private int y;
+
+        Direction(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
 }
--- src/main/java/ua/javarush/island/Location.java
+++ src/main/java/ua/javarush/island/Location.java
@@ -1,4 +1,33 @@
 package ua.javarush.island;
 
+import ua.javarush.living_beings.LivingBeing;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 public class Location {
+    private final int x;
+    private final int y;
+    private final Map<Class<? extends LivingBeing>, List<LivingBeing>> livingBeings;
+
+    public Location(int x, int y) {
+        this.x = x;
+        this.y = y;
+        this.livingBeings = new ConcurrentHashMap<>();
+    }
+
+    public void addLivingBeing(LivingBeing livingBeing){
+        livingBeings
+                .computeIfAbsent(livingBeing.getClass(), key -> new ArrayList<>()).add(livingBeing);
+    }
+
+    public void removeLivingBeing(LivingBeing livingBeing){
+        Class<? extends LivingBeing> entityClass = livingBeing.getClass();
+        livingBeings.computeIfPresent(entityClass, (k, v) -> {
+            v.remove(livingBeing);
+            return v;
+        });
+    }
 }
