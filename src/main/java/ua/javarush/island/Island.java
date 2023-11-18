3c1fa1f132681e4f7de9bc1ff89b505d1225987f
Bohdan Rudenko <brudenko2804@gmail.com>
2023-11-18 18:53:09 +0200

    Remove inner enum Direction from the Island class and make it separate

--- src/main/java/ua/javarush/island/Island.java
+++ src/main/java/ua/javarush/island/Island.java
@@ -6,19 +6,4 @@ public class Island {
     public Island(int x, int y) {
         locations = new Location[x][y];
     }
-
-    public enum Direction{
-        LEFT(1,0),
-        RIGHT(-1, 0),
-        UP(0, 1),
-        DOWN(0, -1);
-
-        private int x;
-        private int y;
-
-        Direction(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-    }
 }
