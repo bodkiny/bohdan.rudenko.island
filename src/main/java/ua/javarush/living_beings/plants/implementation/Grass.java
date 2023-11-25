4e378100019b2e30e24b8e1e6eba4fb212409569
Bohdan Rudenko <brudenko2804@gmail.com>
2023-11-25 18:02:08 +0200

    Add classes of living beings and their JSON configuration files

---
+++ src/main/java/ua/javarush/living_beings/plants/implementation/Grass.java
@@ -0,0 +1,8 @@
+package ua.javarush.living_beings.plants.implementation;
+
+import ua.javarush.annotation.Configuration;
+import ua.javarush.living_beings.plants.Plant;
+
+@Configuration(path = "src/main/resources/configuration/living_beings/plants/grass.json")
+public class Grass extends Plant {
+}
